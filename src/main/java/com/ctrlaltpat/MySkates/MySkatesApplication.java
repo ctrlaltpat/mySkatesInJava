25952882d8ab29dbad4f0e7facc6c3a2fdb6946c
Patrick <ctrlaltpat@gmail.com>
2025-07-08 22:01:30 +0100

    feat: initial commit

---
+++ src/main/java/com/ctrlaltpat/MySkates/MySkatesApplication.java
@@ -0,0 +1,13 @@
+package com.ctrlaltpat.MySkates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySkatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySkatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctrlaltpat/MySkates/MySkatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctrlaltpat.MySkates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySkatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
